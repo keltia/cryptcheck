d545ee8d4f875154062490f04c9b307d36ab8b7d
Ollivier Robert <roberto@keltia.net>
2018-05-07 15:57:11 +0200

    Tests will be written, promise.

---
+++ imirhil_test.go
@@ -0,0 +1,15 @@
+package imirhil
+
+import "testing"
+
+func TestNewClient(t *testing.T) {
+
+}
+
+func TestClient_GetScore(t *testing.T) {
+
+}
+
+func TestClient_GetDetailedReport(t *testing.T) {
+
+}
