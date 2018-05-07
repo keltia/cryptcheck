f91bbc14469d46f369b8afaae9021e653f3aa343
Ollivier Robert <roberto@keltia.net>
2018-05-07 13:15:14 +0200

    Fold these into Client.

--- utils.go
+++ utils.go
@@ -3,15 +3,15 @@ package imirhil
 import "log"
 
 // debug displays only if fDebug is set
-func debug(str string, a ...interface{}) {
-	if ctx.level >= 2 {
+func (c *Client) debug(str string, a ...interface{}) {
+	if c.level >= 2 {
 		log.Printf(str, a...)
 	}
 }
 
 // debug displays only if fVerbose is set
-func verbose(str string, a ...interface{}) {
-	if ctx.level >= 1 {
+func (c *Client) verbose(str string, a ...interface{}) {
+	if c.level >= 1 {
 		log.Printf(str, a...)
 	}
 }
