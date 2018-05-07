afb4a4bd6ff548493f76cd76b51eb84b3457df95
Ollivier Robert <roberto@keltia.net>
2018-05-07 17:57:27 +0200

    Tests for NewClient().

--- imirhil_test.go
+++ imirhil_test.go
@@ -1,9 +1,42 @@
 package imirhil
 
-import "testing"
+import (
+    "testing"
+    "github.com/stretchr/testify/require"
+    "github.com/stretchr/testify/assert"
+)
 
 func TestNewClient(t *testing.T) {
+    c := NewClient(0, false)
 
+    require.NotNil(t, c)
+    require.IsType(t, (*Client)(nil), c)
+    require.NotNil(t, c.client)
+
+    assert.Equal(t, 0, c.level)
+    assert.False(t, c.refresh)
+}
+
+func TestNewClient2(t *testing.T) {
+    c := NewClient(1, false)
+
+    require.NotNil(t, c)
+    require.IsType(t, (*Client)(nil), c)
+    require.NotNil(t, c.client)
+
+    assert.Equal(t, 1, c.level)
+    assert.False(t, c.refresh)
+}
+
+func TestNewClient3(t *testing.T) {
+    c := NewClient(0, true)
+
+    require.NotNil(t, c)
+    require.IsType(t, (*Client)(nil), c)
+    require.NotNil(t, c.client)
+
+    assert.Equal(t, 0, c.level)
+    assert.True(t, c.refresh)
 }
 
 func TestClient_GetScore(t *testing.T) {
