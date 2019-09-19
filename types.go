744207aef1b8d731ad9f6d6c4a7d132be0e0a269
Ollivier Robert <roberto@keltia.net>
2019-09-19 15:21:47 +0200

    API data change, "201909":
    - Added Result struct
    - Move data round
    - New Pending field

--- types.go
+++ types.go
@@ -10,11 +10,12 @@
 // 20171204 add ID to struct Report
 // 20180502 added two fields in the report top struct
 // 20180904 added Host.Error.
+// 20190914 new incompatible API but same Endpoint :(
+//          data moves around, etc.
 
 package cryptcheck
 
 import (
-	"net/http"
 	"time"
 )
 
@@ -74,13 +75,22 @@ type Host struct {
 
 // Report describes the details for the crypto
 type Report struct {
-	Hosts []Host
-	Date  time.Time `json:"date"`
-	ID    struct {
+	ID struct {
 		Oid string `json:"$oid"`
 	} `json:"_id"`
-	Host string
-	Port int
+	Service   string
+	Host      string
+	Port      int
+	Pending   bool
+	Result    Result
+	CreateAt  time.Time `json:"create_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// Result holds the actual results, host-per-host
+type Result struct {
+	Date  time.Time `json:"date"`
+	Hosts []Host
 }
 
 // Client is used to store proxyauth & other internal state
